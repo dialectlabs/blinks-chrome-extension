f821ed38dec6d0d58a34176f3f06bcb8fa27c3aa
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-05-06 19:51:09 +0300

    configure tailwind for content script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  safelist: process.env.NODE_ENV === 'development' ? [{ pattern: /./ }] : [],
   theme: {
     extend: {},
   },
