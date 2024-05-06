aa246c00798f039e74ad0ed52a19523bc66ec0df
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-05-06 17:08:11 +0300

    setup tailwind for popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Popup } from './Popup'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <Popup />
+    <div className="text-cyan-300">nothing to see here yet</div>
   </React.StrictMode>,
-)
+);
