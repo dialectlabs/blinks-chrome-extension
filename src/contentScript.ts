f3dd362521d072b2a16420e52ce3fc2c3f81e17a
Filipp Sher <filipp.sher@gmail.com>
2024-06-11 12:03:27 +0300

    feat: use actions-ui

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,3 +1,19 @@
-import { setupTwitterObserver } from './lib/ext/twitter';
+import '@dialectlabs/actions-ui/index.css';
+import { setupTwitterObserver } from '@dialectlabs/actions-ui/ext/twitter';
+import { ActionConfig } from '@dialectlabs/actions-ui';
 
-setupTwitterObserver();
+const adapter = new ActionConfig('', {
+  signTransaction: (tx: string) =>
+    chrome.runtime.sendMessage({
+      type: 'sign_transaction',
+      payload: {
+        txData: tx,
+      },
+    }),
+  connect: () =>
+    chrome.runtime.sendMessage({
+      type: 'connect',
+    }),
+});
+
+setupTwitterObserver(adapter);
