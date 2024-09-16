160c69cf325901d744eafea5ae60357df6c1df81
Filipp Sher <filipp.sher@gmail.com>
2024-09-16 19:35:16 +0300

    fix: rename env variable

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,6 +1,6 @@
 import posthog from 'posthog-js';
 
-const postHogClient = posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
+const postHogClient = posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_POSTHOG_HOST,
   person_profiles: 'identified_only',
 });
